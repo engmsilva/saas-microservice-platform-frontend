4d539e4654379af8e42f760de9c018d2053d5acd
Marcelo Ribeiro da Silva <eng.msilva@yahoo.com.br>
2024-11-26 14:29:09 -0300

    Update tailwind.config.js [BoltSync]

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        monaco: {
+          dark: '#1e1e1e',
+          light: '#ffffff'
+        }
+      }
+    },
   },
   plugins: [],
 };
