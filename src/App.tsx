731bf41a4f520224c73d7a4da80bdc8001df7400
Marcelo Ribeiro da Silva <eng.msilva@yahoo.com.br>
2024-11-28 16:43:39 -0300

    Update src/App.tsx [BoltSync]

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { WorkflowEditor } from './components/workflow/WorkflowEditor';
 import { ThemeToggle } from './components/ThemeToggle';
 import { ThemeProvider } from './contexts/ThemeContext';
+import { Toaster } from 'sonner';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <main>
           <WorkflowEditor />
         </main>
+        <Toaster richColors />
       </div>
     </ThemeProvider>
   );
